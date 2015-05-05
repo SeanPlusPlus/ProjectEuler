c126bdc2c585b0a66a5b8fab03db896347dcecd4
Sean Stephenson <sms@pixar.com>
2015-05-04 22:58:35 -0700

    add 3/5 multiples answer

---
+++ 001_multiples_of_3_and_5.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dustin/go-humanize"
+)
+
+func main() {
+	var limit int = 1000
+	var sum int = 0
+	for i := 1; i < limit; i++ {
+		if i%3 == 0 {
+			sum += i
+		}
+		if i%5 == 0 {
+			sum += i
+		}
+	}
+	var sum64 = int64(sum)
+	fmt.Printf("%s\n", humanize.Comma(sum64))
+}
