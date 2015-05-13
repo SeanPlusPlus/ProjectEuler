754beb34d298b31d29f98a68417c06f7ac820827
Sean Stephenson <sms@pixar.com>
2015-05-12 21:48:10 -0700

    check all nums and save state

--- 004_largest_palindrome.go
+++ 004_largest_palindrome.go
@@ -12,33 +12,51 @@ import (
 //
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-func isPalindrome(num int, distance int) bool {
-	s := strconv.Itoa(num)
-	distance := 0
-	start := string(s[distance])
-	end := string(s[len(s)-1-distance])
+func isPalindrome(num int, distance int) int {
 
-	// return true
+	num_str := strconv.Itoa(num)
+	left_idx := distance
+	right_idx := len(num_str) - 1 - distance
 
-	// check next value
-	if start == end {
-		return number + isPalindrome(number-1)
+	if (right_idx - left_idx) < 1 {
+		fmt.Println(num_str)
+		return num
+	}
+
+	left_char := string(num_str[left_idx])
+	right_char := string(num_str[right_idx])
+
+	if left_char == right_char {
+		distance += 1
+		return isPalindrome(num, distance)
 	} else {
-		return false
+		fmt.Println(num_str)
+		return 0
 	}
 }
 
 func main() {
 
+	largest_palindrome := 0
 	x := 998
 	y := 999
 	for {
+		fmt.Println(x)
+		fmt.Println(y)
 		answer := isPalindrome((x * y), 0)
-		if answer == true {
-			break
-		} else {
-			x -= 1
+		if answer != 0 {
+			if answer > largest_palindrome {
+				largest_palindrome = answer
+			}
+		}
+		x -= 1
+		if x == 100 {
 			y -= 1
+			x = y - 1
+		}
+		if y == 101 {
+			fmt.Println(largest_palindrome)
+			return
 		}
 	}
 
