31bc7a800c96b4ea2711f78e95098f953987bf49
Sean Stephenson <sms@pixar.com>
2015-07-18 15:57:08 -0700

    add function tp generate primes

--- 010_summation_of_primes.go
+++ 010_summation_of_primes.go
@@ -8,6 +8,38 @@ import "fmt"
 //
 // Find the sum of all the primes below two million.
 
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func Generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i // Send 'i' to channel 'ch'.
+	}
+}
+
+// Copy the values from channel 'in' to channel 'out',
+// removing those divisible by 'prime'.
+func Filter(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in // Receive value from 'in'.
+		if i%prime != 0 {
+			out <- i // Send 'i' to 'out'.
+		}
+	}
+}
+
+// The prime sieve: Daisy-chain Filter processes.
 func main() {
-	fmt.Println("hi")
+	ch := make(chan int) // Create a new channel.
+	go Generate(ch)      // Launch Generate goroutine.
+	i := 0
+	for {
+		prime := <-ch
+		fmt.Println(prime)
+		if prime > 30 {
+			return
+		}
+		ch1 := make(chan int)
+		go Filter(ch, ch1, prime)
+		ch = ch1
+		i += 1
+	}
 }
