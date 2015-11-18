678ef52e5df84dc97ffabfd964c98ee550fe2afc
Sean Stephenson <sms@pixar.com>
2015-11-17 20:37:35 -0800

    distinct powers YO

--- 030_distinct_powers.py
+++ 030_distinct_powers.py
@@ -2,26 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import time
-from decimal import *
 
 # https://projecteuler.net/problem=29
 
 def main():
-    ITER = 101
-    s = set()
-    with localcontext(Context(prec=201)) as ctx:
-        for n in range(2,ITER):
-            for exp in range(2,ITER):
-                x = ctx.power(n,exp)
-                print x
-                s.add(x)
-            print ''
+    ITER = 1000000
+    exp =  5
+    total = 0
+    for i in range(2, ITER):
+        i_sum = 0
+        for c in list(str(i)):
+            i_sum += pow( int(c), exp)
+        if i == i_sum:
+            print [c for c in list(str(i))], [pow (int(c), exp) for c in list(str(i))]
+            print i
+            total += i
+                
 
-    print 'distinct terms: \n', len(s)
+    print "total:\n", total
 
 if __name__ == '__main__':
     start_time = time.time()
     main()
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
 
-    # 0.53 seconds
+    # 4.60seconds
