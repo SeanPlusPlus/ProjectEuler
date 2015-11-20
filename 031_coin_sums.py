14b263cbb04135f786fd7341c0fc0ae531cf54a6
Sean Stephenson <sms@pixar.com>
2015-11-19 21:50:15 -0800

    add coin sums

--- 031_coin_sums.py
+++ 031_coin_sums.py
@@ -6,11 +6,15 @@ import time
 # https://projecteuler.net/problem=31
 
 def main():
-    print "coin sums"
+    total = 200
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    ways = []
+
+     
 
 if __name__ == '__main__':
     start_time = time.time()
     main()
-    print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
+    #print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
 
     # 4.60seconds
