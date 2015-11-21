fab94a25463b9fa451136cfc5aab7782070c50a4
Sean Stephenson <sms@pixar.com>
2015-11-21 12:31:00 -0800

    solve pandigital sum problem

--- 032_pandigital_products.py
+++ 032_pandigital_products.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import itertools as it
 import time
 
 # https://projecteuler.net/problem=32
+
+ITER = 10
      
 def main():
-    s = set(i for i in range(10))
-    print s
+    digits = set([str(n) for n in range(1,ITER)])
+    products = set()
+
+    pairs = [[2,3],[1,4]]
+    for number in pairs:
+        multipliers = it.permutations(digits,number[0])
+        for multiplier in multipliers:
+
+            # get 2 digit permutations of the remaining digits 
+            li = list(digits - set(multiplier))
+            multiplicands = it.permutations(li,number[1])
+            for multiplicand in multiplicands:
+
+                a = int(''.join(multiplier))
+                b = int(''.join(multiplicand))
+                product = a * b
+                product_tuple = tuple(str(product))
+                if len(product_tuple) == 4:
+                    s = set(multiplier + multiplicand + tuple(str(product)))
+                    if len(s) == 9:
+                        if '0' not in s:
+                            print a,b,product,s 
+                            products.add(product)
+
+    print products
+    print sum(products)
 
 if __name__ == '__main__':
     start_time = time.time()
