0381e1b7c139f1d67cf53c33b4c7633c9ddb0c04
Sean Stephenson <sms@pixar.com>
2015-11-21 11:37:32 -0800

    add start of pandigital problem

--- 031_coin_sums.py
+++ 031_coin_sums.py
@@ -4,17 +4,22 @@
 import time
 
 # https://projecteuler.net/problem=31
-
-def main():
-    total = 200
-    coins = [1, 2, 5, 10, 20, 50, 100, 200]
-    ways = []
-
+# FULL DISCLOSUE: http://blog.dreamshire.com/project-euler-31-solution/
      
+def main():
+    target = 200
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    ways = [1] + [0 for n in range(target)]
+
+    for coin in coins:
+        for i in range(coin, target+1):
+            ways[i] += ways[i-coin]
+
+    print "ways", ways[target]
 
 if __name__ == '__main__':
     start_time = time.time()
     main()
-    #print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
+    print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
 
-    # 4.60seconds
+    # 0 seconds
---
+++ 032_pandigital_products.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+# https://projecteuler.net/problem=32
+     
+def main():
+    s = set(i for i in range(10))
+    print s
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    #print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
+
+    # 0 seconds
