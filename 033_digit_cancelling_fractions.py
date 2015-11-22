6cc5e7b6f6b0715d8826a81596d48f2beb7c7869
Sean Stephenson <sms@pixar.com>
2015-11-21 16:45:51 -0800

    almost done with product fraction

---
+++ 033_digit_cancelling_fractions.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+# https://projecteuler.net/problem=33
+
+ITER = 100
+
+def lists_overlap_once(a, b):
+    return len(set(a).intersection(set(b))) == 1
+     
+def main():
+    ans = []
+    digits = [str(n) for n in range(11,ITER) if (n % 10) != 0]
+    for n in digits:
+        for d in digits:
+
+            n_as_li = list(n)
+            d_as_li = list(d)
+
+            if lists_overlap_once(n_as_li, d_as_li):
+                common_digit = list(set(n_as_li).intersection(set(d_as_li)))[0]
+                product_both = float(n) / float(d)
+
+                #print n_as_li, d_as_li, common_digit, product_both
+
+                n_as_li.remove(common_digit)
+                n_single_digit = n_as_li[0]
+
+                d_as_li.remove(common_digit)
+                d_single_digit = d_as_li[0]
+
+                product_single = float(n_single_digit) / float(d_single_digit)
+
+                if product_single < 1:
+                    if product_single == product_both:
+                        ans.append([n, d])
+
+    print ans
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    #print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
+
+    # 0.07 seconds
