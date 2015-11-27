84fa28ab36e5e248967a558b0cdf55df2d57a503
Sean Stephenson <sms@pixar.com>
2015-11-26 19:44:47 -0800

    refactor int2base

--- 036_double_base_palindromes.py
+++ 036_double_base_palindromes.py
@@ -6,29 +6,29 @@ import string
 
 # https://projecteuler.net/problem=36
 
+DIGS = string.digits + string.letters
+
+def int2base(x, base):
+    if x < 0: sign = -1
+    elif x == 0: return DIGS[0]
+    else: sign = 1
+    x *= sign
+    digits = []
+    while x:
+        digits.append(DIGS[x % base])
+        x /= base
+    if sign < 0:
+        digits.append('-')
+    digits.reverse()
+    return ''.join(digits)
+
+def is_palindrome(string):
+    for i,char in enumerate(string):
+        if char != string[-i-1]:
+            return False
+    return True
+
 def main():
-
-    def is_palindrome(string):
-        for i,char in enumerate(string):
-            if char != string[-i-1]:
-                return False
-        return True
-
-    digs = string.digits + string.letters
-    def int2base(x, base):
-        if x < 0: sign = -1
-        elif x == 0: return digs[0]
-        else: sign = 1
-        x *= sign
-        digits = []
-        while x:
-            digits.append(digs[x % base])
-            x /= base
-        if sign < 0:
-            digits.append('-')
-        digits.reverse()
-        return ''.join(digits)
-
     ans = 0
     for n in range(0,1000001):
         n_str = str(n)
@@ -37,7 +37,6 @@ def main():
             if is_palindrome(n_bin):
                 print n, n_bin
                 ans += n
-    
     print "ans:", ans
 
 if __name__ == '__main__':
@@ -45,4 +44,4 @@ if __name__ == '__main__':
     main()
     print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
 
-    # 290.45 seconds
+    # 6.12 seconds
