d26006d07290f99ef16faf0d2f1df2a1820832b6
Sean Stephenson <sms@pixar.com>
2015-11-27 10:53:25 -0800

    scaffold out trum funcs

--- 037_truncatable_primes.py
+++ 037_truncatable_primes.py
@@ -5,24 +5,20 @@ import time
 
 # https://projecteuler.net/problem=37
 
-def trim_left(s):
+def trim_str(s, direction="left"):
     print s
-    trimmed = s[1:]
+    if direction == "left":
+        trimmed = s[1:]
+    else:
+        trimmed = s[:-1]
     if len(trimmed) == 0:
         return
     return trim_left(trimmed)
 
-def trim_right(s):
-    print s
-    trimmed = s[:-1]
-    if len(trimmed) == 0:
-        return
-    return trim_right(trimmed)
-
 def main():
     s = "3797"
-    trim_left(s)
-    trim_right(s)
+    trim_str(s)
+    trim_str(s, direction="right")
 
 if __name__ == '__main__':
     start_time = time.time()
