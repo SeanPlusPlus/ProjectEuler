c36a1bd7ef7478f7cd8fd46ed3cdfd3de822da0a
Sean Stephenson <sms@pixar.com>
2015-11-28 14:59:14 -0800

    tighten up naming conventions

--- 038_pandigital_multiples.py
+++ 038_pandigital_multiples.py
@@ -8,25 +8,25 @@ import time
 def concatenated_product(products):
     s = "".join([str(e) for e in products])
     return {
-        "sorted": "".join(sorted( s )),
-        "raw": int(s)
+        "sorted_string": "".join(sorted( s )),
+        "integer": int(s)
     }
 
 def main():
-    pan_digits_sorted = "".join([str(e) for e in range(1,10)])
+    pan_digits_sorted_string = "".join([str(e) for e in range(1,10)])
     greatest = 0
-    targets = range(1,10001)
-    for target in targets:
+    nums = range(1,10001)
+    for num in nums:
         sequences = range(2,11)
         for s in sequences:
-            products  = []
+            products = []
             for i in range(1, s):
-                products.append(i * target)
+                products.append(i * num)
             product = concatenated_product(products) 
-            if product["sorted"] == pan_digits_sorted:
-                print "taget:", target, "range bound:", s, "product:", product["raw"]
-                if product["raw"] > greatest:
-                    greatest = product["raw"]
+            if product["sorted_string"] == pan_digits_sorted_string:
+                print "num:", num, "range: 1 ->", s, "product:", product["integer"]
+                if product["integer"] > greatest:
+                    greatest = product["integer"]
 
     print "greatest:", greatest
 
