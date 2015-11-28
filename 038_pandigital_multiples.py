b296aacf8612a25f1b0d2291d3863cf8925a3f5c
Sean Stephenson <sms@pixar.com>
2015-11-28 14:52:14 -0800

    include time on concat prod

--- 038_pandigital_multiples.py
+++ 038_pandigital_multiples.py
@@ -6,14 +6,14 @@ import time
 # https://projecteuler.net/problem=38
 
 def concatenated_product(products):
-    s = ''.join([str(e) for e in products])
+    s = "".join([str(e) for e in products])
     return {
-        "sorted": ''.join(sorted( s )),
+        "sorted": "".join(sorted( s )),
         "raw": int(s)
     }
 
 def main():
-    pan_digits_sorted = ''.join([str(e) for e in range(1,10)])
+    pan_digits_sorted = "".join([str(e) for e in range(1,10)])
     greatest = 0
     targets = range(1,10001)
     for target in targets:
@@ -24,16 +24,16 @@ def main():
                 product = i * target
                 products.append(product)
             concatenated = concatenated_product(products) 
-            if concatenated['sorted'] == pan_digits_sorted:
-                print "taget", target, "range bound", s, "product", concatenated['raw']
-                if concatenated['raw'] > greatest:
-                    greatest = concatenated['raw']
+            if concatenated["sorted"] == pan_digits_sorted:
+                print "taget:", target, "range bound:", s, "product:", concatenated["raw"]
+                if concatenated["raw"] > greatest:
+                    greatest = concatenated["raw"]
 
     print "greatest:", greatest
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_time = time.time()
     main()
     print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
 
-    # 0 seconds
+    # 0.64 seconds
