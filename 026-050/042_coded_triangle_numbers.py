bf7dadd022f7a6dd3cd278716d37be280596eaae
Sean Stephenson <sms@pixar.com>
2015-12-03 21:04:09 -0800

    parse words list

--- 026-050/042_coded_triangle_numbers.py
+++ 026-050/042_coded_triangle_numbers.py
@@ -5,19 +5,14 @@ import time
 
 # https://projecteuler.net/problem=42
 
-
 def main():
-
     f = file('042_words.txt')
     words = f.readlines()[0].replace('"','').split(',')
     print "words", words, len(words)
-
-
-    
 
 if __name__ == "__main__":
     start_time = time.time()
     main()
     print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
 
-    # 11.20 seconds
+    # 0 seconds
