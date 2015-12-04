aabc3ca995294526b33ce5a71d7b68e63e6c017c
Sean Stephenson <sms@pixar.com>
2015-12-03 21:27:32 -0800

    solve for triangle words

--- 026-050/042_coded_triangle_numbers.py
+++ 026-050/042_coded_triangle_numbers.py
@@ -5,10 +5,30 @@ import time
 
 # https://projecteuler.net/problem=42
 
-def main():
+def get_words():
     f = file('042_words.txt')
-    words = f.readlines()[0].replace('"','').split(',')
-    print "words", words, len(words)
+    words = f.readlines()[0].replace('"','').rstrip().split(',')
+    return words
+
+def triangle(n):
+    return int((.5 * n) * (n + 1))
+
+def main():
+    triangles = [triangle(n) for n in range(1,1000)]
+    letters = {}
+    for idx, n in enumerate(range(65,91)):
+        letters[chr(n)] = idx + 1
+
+    words = get_words()
+    triangle_words = 0
+    for word in words:
+        word_sum = 0
+        for letter in word:
+            word_sum += letters[letter]
+        if word_sum in triangles:
+            triangle_words += 1
+
+    print triangle_words
 
 if __name__ == "__main__":
     start_time = time.time()
