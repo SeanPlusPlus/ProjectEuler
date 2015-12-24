4823b9a6f38bcac0a04cfa605d16b1be65901400
Sean Stephenson <sms@pixar.com>
2015-12-24 02:20:08 -0800

    add self pow

--- 026-050/048_self_powers.py
+++ 026-050/048_self_powers.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time, math
+import time, math, operator, functools
 
 # https://projecteuler.net/problem=48
 
-def pow_reduce(n,k, count=0):
-    count += 1
-    n = (n * n) % 100
-    b = "{0:b}".format(n)
-    print "n:", n, "b:", b, "len b:", len(b)
-    if count == k:
-        print n
+def pow_reduce(n,k,i=2):
+    n = (n * n) % int(math.pow(10,10))
+    if i > k:
         return n
-    return pow_reduce(n,k,count)
+    i += 1
+    return pow_reduce(n,k,i)
 
+def get_exp(n,k):
+    li = []
+    b = "{0:b}".format(k)
+    l = len(b) - 1
+    for idx, digit in enumerate(b):
+        if digit != "0":
+            k = l - idx
+            if k != 0:
+                p = pow_reduce(n,k)
+                li.append(p)
+            else:
+                li.append(n)
+    ans = functools.reduce(operator.mul, li, 1)
+    return ans % int(math.pow(10,10))
 
 def main():
-    num = 2
-    exp = 4
-    pow_reduce(num,exp)
+    li = [] 
+    for num in range(1,1001):
+        li.append(get_exp(num,num))
+    print sum(li) % int(math.pow(10,10))
 
 
 if __name__ == "__main__":
