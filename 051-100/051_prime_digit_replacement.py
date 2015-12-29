fef008275657e9c6b90d4673bc5eb0fbe7084605
Sean Stephenson <sms@pixar.com>
2015-12-29 12:48:11 -0800

    refactor to has three adjacent

--- 051-100/051_prime_digit_replacement.py
+++ 051-100/051_prime_digit_replacement.py
@@ -19,15 +19,11 @@ def has_three_repeating_adjacent(s):
 def main():
     limit = 1000000
     primes = [p for p in get_primes(limit) if p > 99999]
-    print "first prime:", primes[0], has_three_repeating_adjacent(primes[0])
     r = primes[random.randint(0,len(primes))]
+    print "first prime:", primes[0], has_three_repeating_adjacent(primes[0])
     print "rand prime: ", r, has_three_repeating_adjacent(r)
     print "last prime: ", primes[-1], has_three_repeating_adjacent(primes[-1])
     print "total prime:", len(primes)
-
-
-
-
 
     # for prime in primes:
         # if has_three_repeating_adjacent(prime):
