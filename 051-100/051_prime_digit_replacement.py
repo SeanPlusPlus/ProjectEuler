e628a5a76fb42a0d988b629efc960c79d97dad6b
Sean Stephenson <sms@pixar.com>
2015-12-29 14:31:32 -0800

    fuck my life

--- 051-100/051_prime_digit_replacement.py
+++ 051-100/051_prime_digit_replacement.py
@@ -3,31 +3,42 @@
 
 import time, random
 import itertools as it
+from collections import Counter
 from primes import get_primes
 
 # https://projecteuler.net/problem=51
 # 6 digit prime with exactly 3 repeating digits
 
-def has_three_repeating_adjacent(s):
-    indices = []
-    s = str(s)
-    for i in range(4):
-        if s[i] == s[i+1] == s[i+2]:
-            indices.append(i)
-    return indices
+
+def has_three_repeating_digits(s):
+    indices = list(str(s))
+    counts = dict(Counter(indices))
+    for k, v in counts.iteritems():
+        if v == 3:
+            if int(k) < 3:
+                return k
+    return False
+
 
 def main():
     limit = 1000000
     primes = [p for p in get_primes(limit) if p > 99999]
     r = primes[random.randint(0,len(primes))]
-    print "first prime:", primes[0], has_three_repeating_adjacent(primes[0])
-    print "rand prime: ", r, has_three_repeating_adjacent(r)
-    print "last prime: ", primes[-1], has_three_repeating_adjacent(primes[-1])
-    print "total prime:", len(primes)
 
-    # for prime in primes:
-        # if has_three_repeating_adjacent(prime):
-            # print prime
+    for p in primes:
+        n = has_three_repeating_digits(p)
+        if n:
+            li = [str(i) for i in range(int(n)+1, 10)]
+            s = str(p)
+            series = [p]
+            for el in li:
+                i = int(s.replace(n,el))
+                if i in primes:
+                    series.append(i)
+            print p, "n:" + n, li, s, series
+            if len(series) == 8:
+                return
+
 
 if __name__ == "__main__":
     start_time = time.time()
