ab4a885aadec78b7fc006f55f94175854016e6f5
Sean Stephenson <sms@pixar.com>
2015-12-29 12:47:53 -0800

    refactor to has three adjacent

--- 051-100/051_prime_digit_replacement.py
+++ 051-100/051_prime_digit_replacement.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
+import time, random
 import itertools as it
 from primes import get_primes
 
 # https://projecteuler.net/problem=51
+# 6 digit prime with exactly 3 repeating digits
+
+def has_three_repeating_adjacent(s):
+    indices = []
+    s = str(s)
+    for i in range(4):
+        if s[i] == s[i+1] == s[i+2]:
+            indices.append(i)
+    return indices
 
 def main():
-    limit = 100000
-    primes = get_primes(limit)
-    print "last prime", primes[-1]
+    limit = 1000000
+    primes = [p for p in get_primes(limit) if p > 99999]
+    print "first prime:", primes[0], has_three_repeating_adjacent(primes[0])
+    r = primes[random.randint(0,len(primes))]
+    print "rand prime: ", r, has_three_repeating_adjacent(r)
+    print "last prime: ", primes[-1], has_three_repeating_adjacent(primes[-1])
+    print "total prime:", len(primes)
 
-    series = []
-    for n in xrange(10):
-        s = str(n)
-        i = "56" + s + s + "3"
-        if int(i) in primes:
-            series.append(i)
 
-    print len(series), series 
+
+
+
+    # for prime in primes:
+        # if has_three_repeating_adjacent(prime):
+            # print prime
 
 if __name__ == "__main__":
     start_time = time.time()
