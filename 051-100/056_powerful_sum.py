b01404b22fe27c4b4b26107054a51d8de3b67fee
Sean Stephenson <sms@pixar.com>
2015-12-30 23:44:14 -0800

    solve power digit sum

---
+++ 051-100/056_powerful_sum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time, math
+
+# https://projecteuler.net/problem=56
+
+def sum_digits(n):
+    i = 0
+    for num in str(n):
+        i += int(num)
+    return i
+
+def main():
+    max_sum = 0
+    for a in range(100):
+        for b in range(100):
+            i = sum_digits(a**b)
+            if i > max_sum:
+                max_sum = i
+    print "max_sum", max_sum
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % "%.2f" % (time.time() - start_time) )
