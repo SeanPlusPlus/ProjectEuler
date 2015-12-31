3a3c49230ce3870f52de36f1cd4eae673af69180
Sean Stephenson <sms@pixar.com>
2015-12-30 22:56:15 -0800

    stub out 55

--- 051-100/055_lychrel.py
+++ 051-100/055_lychrel.py
@@ -5,12 +5,15 @@ import time
 
 # https://projecteuler.net/problem=55
 
+def is_palindrome(n):
+    return False
+
 def flip(n):
     return int(str(n)[::-1])
 
 def main():
     for n in range(10,21):
-        print n, flip(n)
+        print n, flip(n), is_palindrome(n)
 
 if __name__ == "__main__":
     start_time = time.time()
