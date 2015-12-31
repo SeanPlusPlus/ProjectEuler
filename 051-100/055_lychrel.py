c5ae42c38c746a55e4a664f54688fb07c9d2cf3a
Sean Stephenson <sms@pixar.com>
2015-12-30 23:09:16 -0800

    add lychrel

--- 051-100/055_lychrel.py
+++ 051-100/055_lychrel.py
@@ -6,14 +6,28 @@ import time
 # https://projecteuler.net/problem=55
 
 def is_palindrome(n):
+    if n == flip(n):
+        return True
     return False
 
 def flip(n):
     return int(str(n)[::-1])
 
+def lychrel(n, iterations=0):
+    if iterations == 50:
+        return False
+    x = n + flip(n)
+    if not is_palindrome(x):
+        iterations += 1
+        return lychrel(x, iterations)
+    return True
+
 def main():
-    for n in range(10,21):
-        print n, flip(n), is_palindrome(n)
+    i = 0
+    for n in range(10001):
+        if not lychrel(n):
+            i += 1
+    print i
 
 if __name__ == "__main__":
     start_time = time.time()
