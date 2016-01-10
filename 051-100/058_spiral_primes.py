9fd8a6d5778e92f7b9b24a479bdd893cab3c80af
Sean Stephenson <sms@pixar.com>
2016-01-10 14:13:19 -0800

    reset back to 1,7

--- 051-100/058_spiral_primes.py
+++ 051-100/058_spiral_primes.py
@@ -33,18 +33,8 @@ def southeast(first, last, multiplier=[4, 4], li=[]):
 def main():
     primes = get_primes(100)
     print primes
-    # first = 1
-    # last = 7
-    # top_right = northwest(first, last)
-    # bottom_left = southeast(first, last)
-    # nums = top_right + bottom_left
-    # print nums, len(top_right), len(bottom_left)
-    # p = len(set(primes).intersection(nums))
-    # n = len(nums) + 1
-    # print p, n
-    # print float(p) / float(n)
     first = 1
-    last = 9
+    last = 7
     top_right = northwest(first, last)
     bottom_left = southeast(first, last)
     nums = top_right + bottom_left
