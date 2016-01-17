a3e78ac4cc8dd114d4b60af2ad1e69ddb0d951f6
Sean Stephenson <sms@pixar.com>
2016-01-17 15:51:56 -0800

    convert returnc funcs to generators

--- 051-100/058_spiral_primes.py
+++ 051-100/058_spiral_primes.py
@@ -2,47 +2,53 @@
 # -*- coding: utf-8 -*-
 
 import time
+import itertools
 from primes import get_primes
 
 # https://projecteuler.net/problem=58
 
 
-def northwest(first, last, li=[]):
+def northwest():
     li = []
     a = 1
-    for n in range(first, last):
-        a = a + (n * 2)
-        li.append(a)
-    return li
+    for i in itertools.count():
+        a = a + (i * 2)
+        yield a
 
 
-def southeast(first, last, multiplier=[4, 4], li=[]):
+def southeast():
+    multiplier = [4, 4]
     a = 1
     m = 0
-    for n in range(first, last):
+    for i in itertools.count():
         try:
             m = multiplier.pop()
         except IndexError:
             multiplier = [m + 4, m + 4]
             m = multiplier.pop()
         a = a + m
-        li.append(a)
-    return li
+        yield a
 
 
 def main():
     primes = get_primes(100)
+    top_right = northwest()
+    bottom_left = southeast()
+    li = []
+    for n in range(6):
+        li.append(top_right.next())
+        li.append(bottom_left.next())
+    li.append(top_right.next())
     print primes
-    first = 1
-    last = 7
-    top_right = northwest(first, last)
-    bottom_left = southeast(first, last)
-    nums = top_right + bottom_left
-    print nums, len(top_right), len(bottom_left)
-    p = len(set(primes).intersection(nums))
-    n = len(nums) + 1
-    print p, n
-    print float(p) / float(n)
+    print li, len(set(primes) & set(li)), len(li)
+    print float(len(set(primes) & set(li))) / float(len(li))
+
+    # nums = top_right + bottom_left
+    # print nums, len(top_right), len(bottom_left)
+    # p = len(set(primes).intersection(nums))
+    # n = len(nums) + 1
+    # print p, n
+    # print float(p) / float(n)
 
 
 if __name__ == "__main__":
