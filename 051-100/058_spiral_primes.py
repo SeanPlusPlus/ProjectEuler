4dd3ce4a86a8df0d8084ec7f54d718750c0058ff
Sean Stephenson <sms@pixar.com>
2016-03-06 12:44:04 -0800

    getting close

--- 051-100/058_spiral_primes.py
+++ 051-100/058_spiral_primes.py
@@ -31,31 +31,47 @@ def southeast():
 
 
 def main():
-    # primes = get_primes(10000000)
+    primes = get_primes(1000)
+    diag_primes = []
     diags = []
-    threshold = 5  # must be odd to complete one wrap of square
     nw = northwest()
     se = southeast()
     nw.next()  # this is number one, the start of spiral
     for i in itertools.count():
+
         nw_next = nw.next()
+
+        # verify that highest num in spiral in primes set
+        if not (nw_next < primes[-1]):
+            print 'err need more primes'
+            return
+
         se_next = se.next()
+
+        # verify that highest num in spiral in primes set
+        if not (se_next < primes[-1]):
+            print 'err need more primes'
+            return
+
         diags.append(nw_next)
         diags.append(se_next)
         print "nw.next()", nw_next
         print "se.next()", se_next
-        if (i % 2) != 0:
-            # this is a complete spiral
-            print diags
-        if i == threshold:
-            return
 
-    # nums = top_right + bottom_left
-    # print nums, len(top_right), len(bottom_left)
-    # p = len(set(primes).intersection(nums))
-    # n = len(nums) + 1
-    # print p, n
-    # print float(p) / float(n)
+        if nw_next in primes:
+            diag_primes.append(nw_next)
+        if se_next in primes:
+            diag_primes.append(se_next)
+
+        if (i % 2) != 0:
+            print diags  # a complete spiral
+            print diag_primes  # set of primes in diags
+            total_diags = float(len(diags) + 1)
+            total_diag_primes = float(len(diag_primes))
+            print total_diag_primes, "/", total_diags
+
+        if len(diags) == 12:
+            return
 
 
 if __name__ == "__main__":
