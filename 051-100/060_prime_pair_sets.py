7099814f86663ac5b0e5c0e29fef8f1cfb3b4a39
Sean Stephenson <sms@pixar.com>
2016-04-17 15:59:20 -0700

    stringify first, then check

--- 051-100/060_prime_pair_sets.py
+++ 051-100/060_prime_pair_sets.py
@@ -18,23 +18,24 @@ def pairs(nums):
     for num in nums:
         compare = [x for x in nums if x != num]
         for n in compare:
-            concat_front = int(str(num) + str(n))
-            concat_back = int(str(n) + str(num))
+            concat_front = int(num + n)
+            concat_back = int(n + num)
             if not (is_prime(concat_front) and is_prime(concat_back)):
                 return False
     
-    return sum(nums) 
+    return [int (n) for n in nums]
 
 
 def main():
     for p in sieve.gen_primes():
         print p
-        nums = [3, 7, 109, 673] 
+        nums = [str(3), str(7) , str(109)]  # , 673] 
+        p = str(p)
         if p not in nums:
             nums.append(p)
             ans = pairs(nums)
             if ans:
-                print 'answer:', ans
+                print 'answer:', ans, sum(ans)
                 return
     
 
