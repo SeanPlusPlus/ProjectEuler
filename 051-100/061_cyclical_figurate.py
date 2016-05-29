28d1e151d30afaff688222beb927668a53c90a95
Sean <seanmichaelstephenson@gmail.com>
2016-05-29 10:47:48 -0700

    finalize figurate class

--- 051-100/061_cyclical_figurate.py
+++ 051-100/061_cyclical_figurate.py
@@ -7,6 +7,8 @@ def main():
     print [f.square(n) for n in range(1,threshold)]
     print [f.pentagonal(n) for n in range(1,threshold)]
     print [f.hexagonal(n) for n in range(1,threshold)]
+    print [f.heptagonal(n) for n in range(1,threshold)]
+    print [f.octoganol(n) for n in range(1,threshold)]
 
 
 if __name__ == '__main__':
--- 051-100/figurate.py
+++ 051-100/figurate.py
@@ -10,5 +10,11 @@ class Figurate:
         return n * ((3 * n) - 1) / 2
 
     def hexagonal(self, n):
-        return n * (2 * n) - 1
+        return n * ((2 * n) - 1)
+
+    def heptagonal(self, n):
+        return n * ((5 * n) - 3) / 2
+
+    def octoganol(self, n):
+        return n * ((3 * n) - 2)
 
