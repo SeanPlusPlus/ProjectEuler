c0bc075e70fdfc4e9f3d60ee5089fdd4c3d3ae8c
Sean <seanmichaelstephenson@gmail.com>
2016-05-29 10:44:58 -0700

    add figurate class

---
+++ 051-100/061_cyclical_figurate.py
@@ -0,0 +1,13 @@
+from figurate import Figurate
+
+def main():
+    f = Figurate()
+    threshold = 6
+    print [f.triangle(n) for n in range(1,threshold)]
+    print [f.square(n) for n in range(1,threshold)]
+    print [f.pentagonal(n) for n in range(1,threshold)]
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ 051-100/figurate.py
@@ -0,0 +1,11 @@
+class Figurate:
+
+    def triangle(self, n):
+        return n * (n + 1) / 2
+
+    def square(self, n):
+        return n ** 2
+
+    def pentagonal(self, n):
+        return n * ((3 * n) - 1) / 2
+
